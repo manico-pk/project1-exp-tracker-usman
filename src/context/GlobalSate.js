d997e517dfa79c15545ea5ef14f97b5ce9bdb287
manico-pk <62180781+manico-pk@users.noreply.github.com>
2021-03-24 15:22:16 +0500

    Rectify the Error in AppReducer

--- src/context/GlobalSate.js
+++ src/context/GlobalSate.js
@@ -1,7 +1,7 @@
 import React, { createContext, useReducer } from 'react';
 
 // Import the Reducer
-import AppReducer from './AppReducer';
+import { AppReducer } from './AppReducer';
 
 
 //create the inital stste
